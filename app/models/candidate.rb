0c84f71d7bb9ce04d5353dc6138d3e18ad6d7646
Henry <vinsen1109@gmail.com>
2021-01-01 22:00:52 +0800

    add model validates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,8 @@
 class Candidate < ApplicationRecord
+
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :party, presence: true
+  validates :politics, presence: true
+
 end
