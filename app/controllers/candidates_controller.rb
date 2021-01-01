ea14cd0c24d814df7d8f3bcf386e0eddf25c2cd6
Henry <vinsen1109@gmail.com>
2021-01-01 21:50:21 +0800

    add candidate create action & permit

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -7,5 +7,18 @@ class CandidatesController < ApplicationController
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to root_path, notice: '新增候選人成功'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
   end
 end
